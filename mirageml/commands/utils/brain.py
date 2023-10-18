4ff880d576a22e8eaf5ed1d0105ba7fa23e6a3b5
Sree Tripuramallu <sree@mirageml.com>
2023-10-18 16:14:24 -0700

    pass access token in header

--- mirageml/commands/utils/brain.py
+++ mirageml/commands/utils/brain.py
@@ -1,7 +1,10 @@
 import os
 import sys
-from io import StringIO
 import requests
+import keyring
+
+from mirageml.constants import SERVICE_ID
+from io import StringIO
 from sentence_transformers import SentenceTransformer
 
 PACKAGE_DIR = os.path.dirname(__file__)
@@ -10,23 +13,23 @@ os.environ['TRANSFORMERS_CACHE'] = os.path.join(PACKAGE_DIR, 'models')
 def local_get_embedding(text_list, embedding_model_id="BAAI/bge-small-en-v1.5"):
     model_dir = os.path.join(PACKAGE_DIR, 'models', embedding_model_id)
     if not os.path.exists(model_dir):
-        os.makedirs(model_dir, exist_ok=True)   
+        os.makedirs(model_dir, exist_ok=True)
         print("Downloading model to:", model_dir)
         print("This will take a few minutes and only happen once!")
-    
+
     # Suppress stdout/stderr
     original_stdout, original_stderr = sys.stdout, sys.stderr
     sys.stdout, sys.stderr = StringIO(), StringIO()
 
     model = SentenceTransformer(
-        embedding_model_id, 
+        embedding_model_id,
         cache_folder=model_dir
         )
     embeddings = model.encode(text_list, normalize_embeddings=False)
 
     # Restore stdout/stderr
     sys.stdout, sys.stderr = original_stdout, original_stderr
-    
+
     # Convert the embeddings to a list
     embeddings = embeddings.tolist()
     return embeddings
@@ -44,21 +47,25 @@ def local_llm_call(messages, llm_model_id="TheBloke/Llama-2-7b-Chat-GGUF", strea
     sys.stdout, sys.stderr = StringIO(), StringIO()
 
     llm = AutoModelForCausalLM.from_pretrained(
-        "TheBloke/Llama-2-7b-Chat-GGUF", 
-        model_file="llama-2-7b-chat.Q4_K_M.gguf", 
-        model_type="llama", 
+        "TheBloke/Llama-2-7b-Chat-GGUF",
+        model_file="llama-2-7b-chat.Q4_K_M.gguf",
+        model_type="llama",
     )
 
     # Restore stdout/stderr
     sys.stdout, sys.stderr = original_stdout, original_stderr
     formatted_messages = "\n".join([x["content"] for x in messages])
-    
+
     if stream: return llm(formatted_messages, stream=True)
     else: return llm(formatted_messages)
 
 def get_embedding(text_list, model="BAAI/bge-small-en-v1.5", local=False):
     if local: return local_get_embedding(text_list, embedding_model_id=model)
-    response = requests.post('https://mirageml--brain-embed-text.modal.run', json={'text_list': text_list})
+    access_token = keyring.get_password(SERVICE_ID, 'access_token')
+    headers = {
+        "x-access-token": access_token
+    }
+    response = requests.post('https://mirageml--brain-embed-text.modal.run', json={'text_list': text_list}, headers=headers)
     response.raise_for_status()  # Raise an exception if the request failed
     return response.json()['embedding']
 
