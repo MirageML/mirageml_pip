31787734eeeada161f4419736699fd3acec72f2c
Aman Kishore <aman@mirageml.com>
2023-10-18 21:06:09 -0700

    List remote sources

--- mirageml/commands/list_sources.py
+++ mirageml/commands/list_sources.py
@@ -1,10 +1,18 @@
-from .utils.vectordb import list_qdrant_db
+from .utils.vectordb import list_qdrant_db, list_remote_qdrant_db
 
 def list_sources():
     sources = list_qdrant_db()
     if len(sources) == 0:
-        print("No sources. Create a source with `mirageml add source`")
-        return
-    print("Sources:")
-    for source in sources:
-        print(source)
+        print("No local sources. Create a source with `mirageml add source`")
+    else:
+        print("Sources:")
+        for source in sources:
+            print(source)
+    remote_sources = list_remote_qdrant_db()
+    if len(remote_sources) == 0:
+        print("No remote sources. Create a source with `mirageml add source`")
+    else:
+        print("Remote Sources:")
+        for source in remote_sources:
+            print(source)
+
