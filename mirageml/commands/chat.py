8361498eee61c40853b01a748cfd6fec9e3a5082
Aman Kishore <aman@mirageml.com>
2023-10-24 13:47:49 -0700

    QoL: Faster chat pull up if no args

--- mirageml/commands/chat.py
+++ mirageml/commands/chat.py
@@ -22,51 +22,52 @@ config = load_config()
 
 def chat(files: list[str] = [], urls: list[str] = [], sources: list[str] = []):
     # Beginning of the chat sequence
-    index_local = False
-    if "local" in sources:
-        index_local = True
-        sources.remove("local")
-
     transient_sources = []
-    local_sources, remote_sources = get_sources()
-    all_sources = list(set(local_sources + remote_sources))
-    for source in sources:
-        if source not in all_sources:
-            typer.secho(
-                f"Source: {source} does not exist. Please add it with `mirageml add source`",
-                fg=typer.colors.RED,
-                bold=True,
-            )
-            sources.remove(source)
+    if files or urls or sources:
+        index_local = False
+        if "local" in sources:
+            index_local = True
+            sources.remove("local")
 
-    if index_local:
-        from .add_source import add_local_source
-
-        sources.append(add_local_source("local"))
-
-    if files or urls:
-        from .utils.local_source import crawl_files
-        from .utils.web_source import extract_from_url
-
-        with Live(
-            Panel(
-                "Scraping Files and Urls",
-                title="[bold green]Assistant[/bold green]",
-                border_style="green",
-            ),
-            console=console,
-            transient=True,
-        ) as live:
-            for source, extractor in zip(files + urls, [crawl_files] * len(files) + [extract_from_url] * len(urls)):
-                live.update(
-                    Panel(
-                        f"Extracting data from {source}",
-                        title="[bold green]Assistant[/bold green]",
-                        border_style="green",
-                    )
+        local_sources, remote_sources = get_sources()
+        all_sources = list(set(local_sources + remote_sources))
+        for source in sources:
+            if source not in all_sources:
+                typer.secho(
+                    f"Source: {source} does not exist. Please add it with `mirageml add source`",
+                    fg=typer.colors.RED,
+                    bold=True,
                 )
-                data, metadata = extractor(source)
-                transient_sources.append((data, metadata))
+                sources.remove(source)
+
+        if index_local:
+            from .add_source import add_local_source
+
+            sources.append(add_local_source("local"))
+
+        if files or urls:
+            from .utils.local_source import crawl_files
+            from .utils.web_source import extract_from_url
+
+            with Live(
+                Panel(
+                    "Scraping Files and Urls",
+                    title="[bold green]Assistant[/bold green]",
+                    border_style="green",
+                ),
+                console=console,
+                transient=True,
+            ) as live:
+                for source, extractor in zip(files + urls, [crawl_files] * len(files) + [extract_from_url] * len(urls)):
+                    live.update(
+                        Panel(
+                            f"Extracting data from {source}",
+                            title="[bold green]Assistant[/bold green]",
+                            border_style="green",
+                        )
+                    )
+                    data, metadata = extractor(source)
+                    transient_sources.append((data, metadata))
 
     while True:
         chat_history = [{"role": "system", "content": "You are a helpful assistant."}]
@@ -77,18 +78,19 @@ def chat(files: list[str] = [], urls: list[str] = [], sources: list[str] = []):
         while True:
             # Loop for follow-up questions
             try:
-                code_blocks = extract_code_from_markdown(ai_response)
-                if code_blocks:
-                    selected_indices = multiline_input(
-                        "Enter the numbers of the code blocks you want to copy, separated by commas (e.g. '1,3,4') or ask a follow-up. Type reset to search again"
-                    )
-                    try:
-                        selected_indices = list(map(int, selected_indices.split(",")))
-                        copy_code_to_clipboard(code_blocks, selected_indices)
-                        ai_response = ""
-                        continue
-                    except ValueError:
-                        user_input = selected_indices
+                if ai_response:
+                    code_blocks = extract_code_from_markdown(ai_response)
+                    if code_blocks:
+                        selected_indices = multiline_input(
+                            "Enter the numbers of the code blocks you want to copy, separated by commas (e.g. '1,3,4') or ask a follow-up. Type reset to search again"
+                        )
+                        try:
+                            selected_indices = list(map(int, selected_indices.split(",")))
+                            copy_code_to_clipboard(code_blocks, selected_indices)
+                            ai_response = ""
+                            continue
+                        except ValueError:
+                            user_input = selected_indices
                 else:
                     if len(chat_history) == 1:
                         user_input = multiline_input("Chat with Mirage")
