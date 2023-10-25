2e4bd1c0c7cd99a1f7035227405b8a1fc05e6aeb
Sree Tripuramallu <sree@mirageml.com>
2023-10-25 10:59:15 -0700

    add email check during login

--- mirageml/commands/login.py
+++ mirageml/commands/login.py
@@ -1,32 +1,30 @@
 import os
 
 import keyring
+import requests
 import segment.analytics as analytics
 import typer
 
-from ..constants import ANALYTICS_WRITE_KEY, SERVICE_ID, supabase
+from ..constants import ANALYTICS_WRITE_KEY, SERVICE_ID, USER_CHECK_ENDPOINT, supabase
 
 analytics.write_key = ANALYTICS_WRITE_KEY
 
 
 def login():
     try:
-        user_id = keyring.get_password(SERVICE_ID, "user_id")
+        already_exists = False
         email = typer.prompt("Email")
+        if email.strip() != "":
+            already_exists = requests.post(USER_CHECK_ENDPOINT, json={"email": email}).json()["exists"]
+
         password = typer.prompt(
-            "Password", hide_input=True, confirmation_prompt="Confirm Password" if user_id is None else False
+            "Password", hide_input=True, confirmation_prompt="Confirm Password" if not already_exists else None
         )
         response = None
-        try:
-            if user_id:
-                response = supabase.auth.sign_in_with_password({"email": email, "password": password})
-            else:
-                response = supabase.auth.sign_up({"email": email, "password": password})
-        except Exception as e:
-            if str(e) == "User already registered":
-                response = supabase.auth.sign_in_with_password({"email": email, "password": password})
-            else:
-                raise e
+        if already_exists:
+            response = supabase.auth.sign_in_with_password({"email": email, "password": password})
+        else:
+            response = supabase.auth.sign_up({"email": email, "password": password})
 
         user = response.user
         session = response.session
