6093ff293985648607320b9237e69b4906bdf603
Sree Tripuramallu <sree@mirageml.com>
2023-10-25 21:44:11 -0700

    use user_collection_requests for creating remote source over a link

--- mirageml/commands/utils/vectordb.py
+++ mirageml/commands/utils/vectordb.py
@@ -50,69 +50,24 @@ def exists_qdrant_db(collection_name="test"):
 def create_remote_qdrant_db(collection_name, link=None, path=None):
     user_id = keyring.get_password(SERVICE_ID, "user_id")
 
-    def make_request(args):
-        i, curr_data, metadata_value, metadata_length, live = args
-        json_data = {
-            "user_id": user_id,
-            "collection_name": collection_name,
-            "data": [curr_data],
-            "metadata": [metadata_value],
-        }
-        if i == 0:
-            response = requests.post(VECTORDB_CREATE_ENDPOINT, json=json_data, headers=get_headers(), stream=True)
-        else:
-            response = requests.post(VECTORDB_UPSERT_ENDPOINT, json=json_data, headers=get_headers(), stream=True)
+    json = {
+        "user_id": user_id,
+        "collection_name": collection_name,
+        "link": link,
+    }
+    headers = {
+        "apikey": SUPABASE_KEY,
+        "Authorization": f"Bearer {keyring.get_password(SERVICE_ID, 'access_token')}",
+        "Content-Type": "application/json",
+        "Prefer": "return=minimal"
+    }
+    response = requests.post(
+        f"{SUPABASE_URL}/rest/v1/user_collection_requests",
+        json=json,
+        headers=headers,
+    )
+    typer.secho(f"Creating Remote Source: {collection_name}. You will receive an email once its ready", fg=typer.colors.GREEN, bold=True)
 
-        if response.status_code == 200:
-            for chunk in response.iter_lines():
-                # process line here
-                link = chunk.decode("utf-8")
-                live.update(
-                    Panel(
-                        f"{link}",
-                        title="[bold green]Indexer[/bold green]",
-                        border_style="green",
-                    )
-                )
-    if link:
-        json = {
-            "user_id": user_id,
-            "collection_name": collection_name,
-            "link": link,
-        }
-        headers = {
-            "apikey": SUPABASE_KEY,
-            "Authorization": f"Bearer {keyring.get_password(SERVICE_ID, 'access_token')}",
-            "Content-Type": "application/json",
-            "Prefer": "return=minimal"
-        }
-        response = requests.post(
-            f"{SUPABASE_URL}/rest/v1/user_collection_requests",
-            json=json,
-            headers=headers,
-        )
-        typer.secho(f"Creating Remote Source: {collection_name}. You will receive an email once its ready", fg=typer.colors.GREEN, bold=True)
-
-    if path:
-        data, metadata = crawl_files(path)
-        console = Console()
-        with Live(
-            Panel(
-                "Creating Embeddings...",
-                title="[bold green]Indexer[/bold green]",
-                border_style="green",
-            ),
-            console=console,
-            transient=True,
-            auto_refresh=True,
-            vertical_overflow="visible",
-        ) as live:
-            with ThreadPoolExecutor() as executor:
-                args_list = [(i, curr_data, metadata[i], live) for i, curr_data in enumerate(data)]
-                list(executor.map(make_request, args_list))
-
-        typer.secho(f"Created Source: {collection_name}", fg=typer.colors.GREEN, bold=True)
-        set_sources()
     return True
 
 
