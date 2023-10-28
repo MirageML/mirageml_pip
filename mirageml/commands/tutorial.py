84631c2c6928d4c31aeb21f5cd263a55557e5d2c
Aman Kishore <aman@mirageml.com>
2023-10-27 19:49:58 -0700

    Better tutorial

--- mirageml/commands/tutorial.py
+++ mirageml/commands/tutorial.py
@@ -9,9 +9,11 @@ def tutorial():
 
     invoked_alias = sys.argv[0].split("/")[-1]
 
-    typer.secho("\nGreat, you are now logged in.\n", fg=typer.colors.GREEN)
+    typer.secho("\nGreat, you are now logged in.\n", fg=typer.colors.BRIGHT_GREEN)
 
-    typer.secho("Next, let's add a knowledge source for the web!", fg=typer.colors.BLUE)
+    typer.secho(
+        "Next, let's add a knowledge source for the web (ex: https://modal.com/docs)!", fg=typer.colors.BRIGHT_BLUE
+    )
     while True:
         link = input("Link for the source: ")
         if not link.startswith("http"):
@@ -26,39 +28,44 @@ def tutorial():
         source_name = parsed_url.netloc.split(".")[1]
     source_name = input(f"source_name for the source [default: {source_name}]: ") or source_name
     add_source(source_name, link)
-    time.sleep(2)
-    typer.secho("\nYou have added your first source!\n", fg=typer.colors.GREEN)
 
-    # typer.secho("Now, let's add a plugin for additional capabilities:", fg=typer.colors.BLUE)
+    time.sleep(2)
+    typer.secho("\nYou have added your first source!", fg=typer.colors.BRIGHT_GREEN)
+
+    # typer.secho("Now, let's add a plugin for additional capabilities:", fg=typer.colors.BRIGHT_BLUE)
     # add_plugin()
-    # typer.secho("\nPlugin added.\n", fg=typer.colors.GREEN)
+    # typer.secho("\nPlugin added.\n", fg=typer.colors.BRIGHT_GREEN)
 
     time.sleep(2)
     typer.secho(
-        f"When the source is ready you can find it using `{invoked_alias} list sources` and run it with `{invoked_alias} chat -s {source_name}`",
-        fg=typer.colors.BLUE,
+        f"Use `{invoked_alias} chat -s modal` when your source is ready and `{invoked_alias} list sources` to list your sources\n",
+        fg=typer.colors.BRIGHT_GREEN,
     )
 
     time.sleep(2)
     typer.secho(
-        f"When chatting you can use `-f` to specify a file or directory to chat with `{invoked_alias} chat -f <file_or_directory>`",
-        fg=typer.colors.BLUE,
+        f"Use `{invoked_alias} chat -u <link_to_webpage>` to open a chat over a specific url",
+        fg=typer.colors.BRIGHT_BLUE,
     )
 
     time.sleep(2)
     typer.secho(
-        f"When chatting you can use `-u` to specify a url to chat with `{invoked_alias} chat -u <link_to_webpage>`",
-        fg=typer.colors.BLUE,
+        f"Use `{invoked_alias} chat -f <file_or_directory>` to open a chat over a file or directory",
+        fg=typer.colors.BRIGHT_BLUE,
     )
 
     time.sleep(2)
-    typer.secho("Let's chat with the file/directory!", fg=typer.colors.BLUE)
-    file_path = input("Enter a file path or directory: ")
+    typer.secho("Let's chat with a file or directory!", fg=typer.colors.BRIGHT_BLUE)
+
+    from prompt_toolkit import prompt
+    from prompt_toolkit.completion import PathCompleter
+
+    file_path = prompt("Enter the filepath to a file or directory: ", completer=PathCompleter())
 
     chat([file_path])
 
     time.sleep(2)
-    typer.secho("\nAnd that concludes the tutorial! You can now use MirageML for:", fg=typer.colors.GREEN)
+    typer.secho("\nAnd that concludes the tutorial! You can now use MirageML for:", fg=typer.colors.BRIGHT_GREEN)
 
     time.sleep(1)
     typer.echo("- Logging in and managing your account")
@@ -67,9 +74,12 @@ def tutorial():
     typer.echo("- Adding and managing knowledge sources")
 
     time.sleep(1)
+    typer.echo("- Finetuning a custom model")
+
+    time.sleep(1)
     typer.echo("- Adding plugins")
 
     time.sleep(1)
-    typer.echo("- Chatting with your assistant")
+    typer.echo("- Chatting with your context-aware assistant")
 
-    typer.secho(f"\nType '{invoked_alias} --help' for more info on all the commands.\n", fg=typer.colors.YELLOW)
+    typer.secho(f"\nType `{invoked_alias} --help` for more info on all the commands.\n", fg=typer.colors.YELLOW)
