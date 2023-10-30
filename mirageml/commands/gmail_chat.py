8eff8b246970e6b0c52e5d1d930af20fc24e1fc3
Sree Tripuramallu <sree@mirageml.com>
2023-10-29 23:21:20 -0700

    connect endpoints to render generated emails

--- mirageml/commands/gmail_chat.py
+++ mirageml/commands/gmail_chat.py
@@ -1,14 +1,52 @@
 import requests
 import pyperclip
+import keyring
+import queue
+import threading
+from concurrent.futures import ThreadPoolExecutor
 from prompt_toolkit import Application
 from prompt_toolkit.buffer import Buffer
 from prompt_toolkit.key_binding import KeyBindings
 from prompt_toolkit.key_binding.bindings.focus import focus_next, focus_previous
-from prompt_toolkit.layout import VSplit, HSplit, Dimension, Layout, ScrollablePane
-from prompt_toolkit.widgets import Box, TextArea, VerticalLine, SearchToolbar, RadioList
+from prompt_toolkit.layout import VSplit, Dimension, Layout
+from prompt_toolkit.widgets import Box, TextArea, VerticalLine, SearchToolbar, RadioList, HorizontalLine
 
 from rich.console import Console
-from ..constants import get_headers, GMAIL_DATA_ENDPOINT
+from .config import load_config
+from ..constants import get_headers, GMAIL_DATA_ENDPOINT, SERVICE_ID, GMAIL_THREAD_FORMAT_ENDPOINT, LLM_GPT_ENDPOINT
+
+
+def worker(q):
+    while True:
+        item = q.get()
+        if item is None: break
+        thread_id, g = item
+        generate_response(thread_id, g)
+
+def llm_call(headers, user_id, model, messages, temperature):
+    generated_response = requests.post(LLM_GPT_ENDPOINT, headers=headers, json={
+        "user_id": user_id,
+        "temperature": temperature,
+        "messages": messages,
+        "model": model,
+    })
+    generated_message = generated_response.json()
+    return generated_message
+
+def generate_response(thread_id, g):
+    formatted_email_response = requests.post(GMAIL_THREAD_FORMAT_ENDPOINT, headers=g.headers, json={"thread_id": thread_id})
+    formatted_email = formatted_email_response.json()
+    g_messages = []
+    messages = [{"role": "system", "content": 'You are an email assistant designed to help users compose emails in their own unique voice.'}, {"role": "user", "content": f"{formatted_email}"}]
+    with ThreadPoolExecutor() as executor:
+        futures = [executor.submit(llm_call, g.headers, g.user_id, g.model, messages, 0.8 - 0.1 * i) for i in range(3)]
+        for future in futures:
+            g_messages.append(future.result())
+
+    g.third_box_text_area.text = "Generated Response. Use `1`, `2`, `3` to copy the response to your clipboard."
+    g.third_box_text_area.text += "\n----------------------------------------\n"
+    g.third_box_text_area.text += "\n----------------------------------------\n".join(g_messages)
+    g.generated_responses = g_messages
 
 def fetch_initial_gmail_data():
     gmail_data_response = requests.post(GMAIL_DATA_ENDPOINT, headers=get_headers(), json={"page_token": None, "query": ""})
@@ -18,11 +56,17 @@ def fetch_initial_gmail_data():
 class GmailChat:
     threads = []
     page_token = None
-    generated_response = []
+    generated_responses = []
 
-    def __init__(self, threads, page_token):
+    def __init__(self, queue, threads, page_token):
+        self.queue = queue
         self.threads = threads
         self.page_token = page_token
+        self.headers = get_headers()
+        self.user_id = keyring.get_password(SERVICE_ID, "user_id")
+
+        config = load_config()
+        self.model = config["model"]
 
         self.kb = KeyBindings()
 
@@ -36,31 +80,33 @@ class GmailChat:
         @self.kb.add("g")
         def _(event):
             self.third_box_text_area.text = "Generating response..."
+            current_thread_id = self.radios.current_value
+            self.queue.put((current_thread_id, self))
 
         @self.kb.add("1")
         def _(event):
-            if len(self.generated_response) > 0:
-                pyperclip.copy(self.generated_response[0])
+            if len(self.generated_responses) > 0:
+                pyperclip.copy(self.generated_responses[0])
 
         @self.kb.add("2")
         def _(event):
-            if len(self.generated_response) > 1:
-                pyperclip.copy(self.generated_response[1])
+            if len(self.generated_responses) > 1:
+                pyperclip.copy(self.generated_responses[1])
 
         @self.kb.add("3")
         def _(event):
-            if len(self.generated_response) > 2:
-                pyperclip.copy(self.generated_response[2])
+            if len(self.generated_responses) > 2:
+                pyperclip.copy(self.generated_responses[2])
 
         @self.kb.add("4")
         def _(event):
-            if len(self.generated_response) > 3:
-                pyperclip.copy(self.generated_response[3])
+            if len(self.generated_responses) > 3:
+                pyperclip.copy(self.generated_responses[3])
 
         self.create_radio(threads)
         self.create_first_box()
         self.create_second_box()
-        self.create_thrid_box()
+        self.create_third_box()
 
     def fetch_gmail_data(self, page_token=None, query=""):
         gmail_data_response = requests.post(GMAIL_DATA_ENDPOINT, headers=get_headers(), json={"page_token": page_token, query: query})
@@ -94,6 +140,8 @@ class GmailChat:
             self.radios.current_value = new_value
             self.second_box_text_area.text = self.fetch_thread_messages(threads, new_value)
             self.third_box_text_area.text = "Use `g` to generate a response"
+            self.generated_responses = []
+            self.third_box.body = self.third_box_text_area
         self.radios._handle_enter = handle_enter
 
     def create_first_box(self):
@@ -103,7 +151,7 @@ class GmailChat:
         self.second_box_text_area = TextArea(self.fetch_thread_messages(self.threads, self.threads[0]["id"]), read_only=True)
         self.second_box = Box(body=self.second_box_text_area, padding_top=0, padding_bottom=0, padding_left=1, padding_right=1, width=Dimension(weight=38), height=Dimension(weight=90))
 
-    def create_thrid_box(self):
+    def create_third_box(self):
         self.third_box_text_area = TextArea("Use `g` to generate a response", read_only=True)
         self.third_box = Box(body=self.third_box_text_area, padding_top=0, padding_bottom=0, padding_left=1, padding_right=1, width=Dimension(weight=38), height=Dimension(weight=90))
 
@@ -146,6 +194,14 @@ def gmail_chat():
     with console.status("[blue]Loading Gmail data...[/blue]", spinner="dots", spinner_style="blue"):
         data = fetch_initial_gmail_data()
 
-    g = GmailChat(data["threads"], data["next_page_token"])
+    q = queue.Queue()
+    t = threading.Thread(target=worker, args=(q,))
+    t.start()
+    q.join()
+
+    g = GmailChat(q, data["threads"], data["next_page_token"])
     g.create_application()
     g.app.run()
+
+    q.put(None)
+    t.join()
